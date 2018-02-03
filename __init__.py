e697bed386602269dd48f7ef8e7cc6683bcaa9c3
John-Nathan Hill <john.nat.hill@gmail.com>
2018-02-03 11:36:21 +0100

    trying to make the skill more general

--- __init__.py
+++ __init__.py
@@ -7,15 +7,15 @@ from mycroft.util.log import getLogger
 from urllib2 import urlopen
 import paho.mqtt.client as mqtt
 
-__author__ = 'jamiehoward430'
+__author__ = 'john_ee'
 
 LOGGER = getLogger(__name__)
 
-class mymqttskill(MycroftSkill):
+class mqtt_client(MycroftSkill):
 
     def __init__(self):
-        super(mymqttskill, self).__init__(name="mymqttskill")
-       
+        super(mqtt_client, self).__init__(name="mqtt_client")
+
         self.protocol = self.config["protocol"]
 	self.mqttssl = self.config["mqtt-ssl"]
 	self.mqttca = self.config["mqtt-ca-cert"]
@@ -24,26 +24,27 @@ class mymqttskill(MycroftSkill):
 	self.mqttauth = self.config["mqtt-auth"]
 	self.mqttuser = self.config["mqtt-user"]
 	self.mqttpass = self.config["mqtt-pass"]
- 
-    
+
+
     def initialize(self):
         self.load_data_files(dirname(__file__))
         self. __build_single_command()
-        
-        
-    def __build_single_command(self):
-        intent = IntentBuilder("mymqttIntent").require("CommandKeyword").require("ModuleKeyword").require("ActionKeyword").build()
-        self.register_intent(intent, self.handle_single_command)
-        
-    def handle_single_command(self, message):
-        cmd_name = message.data.get("CommandKeyword")
-        mdl_name = message.data.get("ModuleKeyword")
-        act_name = message.data.get("ActionKeyword")
-        dev_name = mdl_name.replace(' ', '_')
-        
-        if act_name:
-            cmd_name += '_' + act_name
 
+
+    def __build_single_command(self):
+        intent = IntentBuilder("writeIntent").require("PublishKeyword").require("Request")
+        self.register_intent(intent, self.handle_write_command)
+        #intent = IntentBuilder("readIntent").require("ReadKeyword").require("TopicKeyword").require("TopicName")
+        #self.register_intent(intent, self.handle_read_command)
+
+    def handle_write_command(self, message):
+        words = message.data['TopicName'].split(' ')
+        payload = words[len(words)-1]
+        topic_name = ""
+        for i in range(len(words)-1):
+            topic_name += words[i]
+            if i != len(words)-2:
+                topic_name += "/" + words[i]
         if (self.protocol == "mqtt"):
 	    mqttc = mqtt.Client("MycroftAI")
 	    if (self.mqttauth == "yes"):
@@ -51,17 +52,19 @@ class mymqttskill(MycroftSkill):
 	    if (self.mqttssl == "yes"):
 		mqttc.tls_set(self.mqttca) #/etc/ssl/certs/ca-certificates.crt
             mqttc.connect(self.mqtthost,self.mqttport)
-	    mqttc.publish("/mycroft/" + cmd_name + "/" + dev_name + "/" + act_name, act_name)
+	    mqttc.publish(topic_name, payload)
 	    mqttc.disconnect()
 	    self.speak_dialog("cmd.sent")
-            LOGGER.info(dev_name + "-" + cmd_name)
+        LOGGER.info("topic : " + topic_name + " payload : " + payload)
 
-	else:
-            self.speak_dialog("not.found", {"command": cmd_name, "action": act_name, "module": dev_name})
-            LOGGER.error("Error: {0}".format(e))
-        
+    #def handle_write_command(self, message):
+        #topic_name = message.data['TopicName'].replace(' ','_')
+
+
+
+
     def stop(self):
         pass
-        
+
 def create_skill():
-    return mymqttskill()
+    return mqtt_client()
